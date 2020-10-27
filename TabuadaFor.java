0f357adf44116cc940f918c7d9a3dfd4cb121103
dllromano <73515734+dllromano@users.noreply.github.com>
2020-10-27 02:56:17 -0300

    Create TabuadaFor.java

---
+++ TabuadaFor.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class TabuadaFor {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		int numero, i, resultado;
+		
+		System.out.println("Digite um número para saber sua tabuada:");
+		numero = leia.nextInt();
+		leia.close();
+		
+		for(i = 0; i < 11; i++) {
+			resultado = i * numero;
+			System.out.println(numero + "x" +i+ "=" + resultado);
+		}
+	}
+
+}
